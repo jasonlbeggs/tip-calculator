cf69675a3fb650e51f54f6884a5d6d4b8c9a23c0
Jason Beggs <jasonlbeggs@gmail.com>
2018-10-27 20:42:03 -0400

    UI updates

--- src/components/TipCalculator.vue
+++ src/components/TipCalculator.vue
@@ -1,19 +1,19 @@
 <template>
-  <div class="flex items-center justify-center container my-8 mx-auto px-4">
-    <div class="w-full lg:w-1/2 bg-grey-lighter rounded shadow">
-      <h1 class="bg-grey-darkest text-white py-2 px-6 rounded-t font-normal text-2xl">Tip Calulator</h1>
+  <div class="flex flex-col items-center justify-center container my-8 mx-auto px-4">
+    <div class="w-full lg:w-1/2 mb-6 bg-grey-lighter rounded-lg shadow-md">
+      <h1 class="bg-teal-dark text-white py-2 px-6 rounded-t-lg font-normal text-2xl">Tip Calulator</h1>
       <div class="py-6 px-6">
         <div class="sm:flex items-center mb-4 sm:mb-8">
           <div class="mb-4 sm:mr-8 flex-1">
             <label for="cost" class="block mb-1 uppercase text-base tracking-wide font-semibold text-grey-darker">Initial Cost:</label>
-            <div class="relative cost bg-white shadow rounded">
-              <input id="cost" v-model.number="cost" class="w-full py-3 pl-8 pr-4" type="text" autofocus>
+            <div class="relative cost bg-white shadow rounded-lg">
+              <input id="cost" v-model.number="cost" class="w-full py-3 pl-8 pr-4 rounded-lg focus:outline-none focus:shadow-outline" type="text" autofocus>
             </div>
           </div>
           <div class="mb-4 sm:mr-8 flex-1">
             <label for="percentage" class="block mb-1 uppercase text-base tracking-wide font-semibold text-grey-darker">Tip Percentage:</label>
-            <div class="relative percentage bg-white shadow rounded">
-              <input id="percentage" v-model.number="percent" class="w-full py-3 px-4" type="text">
+            <div class="relative percentage bg-white shadow rounded-lg">
+              <input id="percentage" v-model.number="percent" class="w-full py-3 px-4 rounded-lg focus:outline-none focus:shadow-outline" type="text">
             </div>
           </div>
         </div>
@@ -29,6 +29,9 @@
         </div>
       </div>
     </div>
+    <div class="text-center">
+      <a href="https://jasonlbeggs.com" class="text-grey-dark no-underline">Made with &hearts; by jasonlbeggs</a>
+    </div>
   </div>
 </template>
 
@@ -36,8 +39,8 @@
 export default {
   data() {
     return {
-      percent: 15,
-      cost: 0,
+      percent: 20,
+      cost: 25,
     };
   },
   computed: {
